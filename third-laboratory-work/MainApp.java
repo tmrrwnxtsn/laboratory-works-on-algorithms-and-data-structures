c83a47cd705ce770fa101fa881933c14d59a7286
tmrrwnxtsn <tmrrwnxtsn@gmail.com>
2021-03-31 17:25:15 +0400

    Add a time dimension

--- third-laboratory-work/MainApp.java
+++ third-laboratory-work/MainApp.java
@@ -1,15 +1,23 @@
+import java.time.Duration;
+import java.time.Instant;
+
 import fractionalKnapsakProblem.FractionalKnapsackProblem;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		FractionalKnapsackProblem theProblem = new FractionalKnapsackProblem(10);
-		theProblem.solveProblem();
-		theProblem.printCurrentProblemState();
+		solveFractionalKnapsackProblem(10);
+	}
+	
+	public static void solveFractionalKnapsackProblem(int numberElements) {
+		FractionalKnapsackProblem theProblem = new FractionalKnapsackProblem(numberElements);
 		
-//		AlgorithmsWithDifferentComplexity theArray = new AlgorithmsWithDifferentComplexity(5);
-//		theArray.makeLargestNumberAlgorithm();
-//		theArray.getMinItem();
-//		theArray.sortInNonDecreasingOrder();
+		Instant start = Instant.now();
+		
+		theProblem.solveProblem();
+		
+		Instant finish = Instant.now();
+		long elapsed = Duration.between(start, finish).toMillis();
+		System.out.println("Решение задачи о рюкзаке для " + numberElements + " элементов занимает " + elapsed +" мс.");
 	}
 }
